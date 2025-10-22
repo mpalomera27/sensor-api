3dcd327ee4eeb3a5e542e55272dec71ac9392550
mpalomera27 <miguel.palomera@tec.mx>
2025-10-22 08:22:43 -0600

    add db connection

--- api/index.py
+++ api/index.py
@@ -1,4 +1,17 @@
 from flask import Flask
+import psycopg2
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env
+load_dotenv()
+
+# Fetch variables
+USER = os.getenv("user")
+PASSWORD = os.getenv("password")
+HOST = os.getenv("host")
+PORT = os.getenv("port")
+DBNAME = os.getenv("dbname")
 
 app = Flask(__name__)
 
@@ -12,4 +25,32 @@ def about():
 
 @app.route('/sensor')
 def sensor():
-    return 'super dupper'
+   # Connect to the database
+    try:
+        connection = psycopg2.connect(
+            user=USER,
+            password=PASSWORD,
+            host=HOST,
+            port=PORT,
+            dbname=DBNAME
+        )
+        print("Connection successful!")
+        
+        # Create a cursor to execute SQL queries
+        cursor = connection.cursor()
+        
+        # Example query
+        cursor.execute("SELECT NOW();")
+        result = cursor.fetchone()
+        print("Current Time:", result)
+    
+        # Close the cursor and connection
+        cursor.close()
+        connection.close()
+        return f"Current Time: {result}"
+    
+    except Exception as e:
+        return f"Failed to connect: {e}"
+    
+    
+
